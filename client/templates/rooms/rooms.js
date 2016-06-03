20fc4cfa6d2939e975de09c8b8a50b592dddf963
akramsaouri <akram_saouri@hotmail.fr>
2016-06-03 22:35:01 +0100

    Cannot create duplicate room

--- client/templates/rooms/rooms.js
+++ client/templates/rooms/rooms.js
@@ -1,17 +1,21 @@
 Meteor.subscribe('rooms');
 
 Template.room.helpers({
-  getRooms(){
-    let roomsName = Rooms.find({"roomTopic": FlowRouter.getParam('topicName')}).fetch();
-    roomsName.map((index) =>  index.cleanRoom = index.roomName.split('-').join(' '))
-    return roomsName;
-  }
-})
+    getRooms(){
+        let roomsName = Rooms.find({"roomTopic": FlowRouter.getParam('topicName')}).fetch();
+        roomsName.map((index) =>  index.cleanRoom = index.roomName.split('-').join(' '))
+        return roomsName;
+    }
+});
+
 Template.room.events({
-  'submit #create-new-room': function(e){
-    e.preventDefault();
-    let roomName = $('#roomName').val();
-    // TODO: CHECK IF ROOM ALREADY IN THIS TOPIC, IF TRUE (BEFORE INSERT IT APPEND AND INDEX)
-    Rooms.insert({"roomName": roomName.split(' ').join('-'), "roomTopic": FlowRouter.getParam('topicName')})
-  }
-})
+    'submit #create-new-room': function (e) {
+        e.preventDefault();
+        let roomName = $('#roomName').val();
+        if (Rooms.findOne({"roomName": roomName.split(' ').join('-')})) {
+            alert('Already Added bb, sir thawa.')
+        } else {
+            Rooms.insert({"roomName": roomName.split(' ').join('-'), "roomTopic": FlowRouter.getParam('topicName')})
+        }
+    }
+});
--- server/publish.js
+++ server/publish.js
@@ -1,7 +1,7 @@
 Meteor.publish('topics', function() {
   return Topics.find();
-})
+});
 
 Meteor.publish('rooms', function() {
   return Rooms.find();
-})
+});
